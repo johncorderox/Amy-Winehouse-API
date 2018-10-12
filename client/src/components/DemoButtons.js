400b5941be39765a4f5c672782ddea8188120ea7
John Cordero <johncorderox@gmail.com>
2018-10-11 20:23:23 -0700

    new functions for state

--- client/src/components/DemoButtons.js
+++ client/src/components/DemoButtons.js
@@ -6,13 +6,22 @@ class DemoButtons extends Component {
     results: undefined
   }
 
+  getSongs = async(e) => {
+
+    e.preventDefault();
+    this.setState({
+      results: "Here are all Beach House Songs: "
+    });
+
+  }
+
   getAlbums = async(e) => {
 
     e.preventDefault();
     this.setState({
-      results: "done!"
-    });
+      results: "Here are all Beach House Albums:"
 
+    });
   }
 
   render() {
@@ -20,8 +29,8 @@ class DemoButtons extends Component {
     return (
       <div>
         <div class="btn-group" role="group" aria-label="Basic example">
-          <button type="button" class="btn btn-secondary" onClick={this.getAlbums}>GET All Songs</button>
-          <button type="button" class="btn btn-secondary">GET All Albums</button>
+          <button type="button" class="btn btn-secondary" onClick={this.getSongs}>GET All Songs</button>
+          <button type="button" class="btn btn-secondary" onClick={this.getAlbums}>GET All Albums</button>
           <button type="button" class="btn btn-secondary">GET All Artists</button>
         </div>
         <div class="api-results" >
