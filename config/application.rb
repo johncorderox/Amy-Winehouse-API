0ae722a6dfa4aeff1abe695e016e73413aa76aba
John Cordero <johncorderox@gmail.com>
2019-05-12 13:47:29 -0700

    rack attack and config init

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,9 @@ module BeachHouseApi
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    # Rack Attack Middleware
+    config.middleware.use Rack::Attack
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
