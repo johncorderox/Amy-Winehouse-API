a5c4a4c8ae033da1bc0e820ec4faf39c52f88d89
John Cordero <johncorderox@gmail.com>
2019-05-13 12:46:31 -0700

    React start

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+import Information from './components/Information';
+
+function App() {
+  return (
+    <div className="App">
+    <style>
+    @import url('https://fonts.googleapis.com/css?family=Bitter');
+    </style>
+    <div className="container">
+      <Information />
+    </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Information.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Information extends Component {
+  render() {
+    return (
+      <div>
+      <div className="row">
+      <div className="col-md-3 offset-md-3"><h1 className="cherry">API</h1></div>
+    </div>
+      </div>
+    );
+  }
+}
+
+export default Information;
