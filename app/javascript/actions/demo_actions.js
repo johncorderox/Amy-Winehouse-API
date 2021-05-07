e5e77070914b7b45028397d1848373e15a711bff
John Cordero <johncorderox@gmail.com>
2021-05-07 12:16:12 -0700

    Redux actions and reducers

--- app/javascript/actions/demo_actions.js
+++ app/javascript/actions/demo_actions.js
@@ -1,8 +1,13 @@
-const GET_ARTIST = "GET_ARTIST";
+export const GET_ARTIST = "GET_ARTIST";
 
 {
   type: GET_ARTIST,
+  console.log("Action called");
   payload: {
     description: 'Get the Artist of id: 1'
   }
+}
+
+export function getArtist() {
+  return { type: GET_ARTIST, artist: a }
 }
--- app/javascript/reducers/reducer.js
+++ app/javascript/reducers/reducer.js
@@ -1,8 +1,15 @@
 
-function reducer(state, action) {
-  if action.type == "GET_ARTIST" {
-    return: [
+import { GET_ARTIST } from '../actions/demo_actions'
 
-    ]
-  }
+function artistReducer(state = [], action)
+{
+    switch(action.type) {
+        case GET_ARTIST:
+            return action.artist;
+
+        default:
+            return state;
+    }
 }
+
+export default artistReducer
